fcb79538bbe188acec93ca171c55e13a734c8462
lexxystore <128270036+lexxystore@users.noreply.github.com>
2023-03-19 09:29:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/krisbotz1/MinecraftPurpleTheme/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/lexxystore/MinecraftPurpleTheme/main/repair.sh)
 }
 
 restoreBackUp(){
