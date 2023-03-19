e36a13217b6bb39650c338c4727032f61a7863a7
lexxystore <128270036+lexxystore@users.noreply.github.com>
2023-03-19 09:44:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ installTheme(){
     echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r MinecraftPurpleTheme
-    git clone https://github.com/krisbotz1/MinecraftPurpleTheme.git
+    git clone https://github.com/lexxystore/MinecraftPurpleTheme.git
     cd MinecraftPurpleTheme
     rm /var/www/pterodactyl/resources/scripts/MinecraftPurpleTheme.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
